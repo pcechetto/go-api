2f90fee988daee5f018cc21a68466aff4defd648
pcechetto <pedrocechetto@hotmail.com>
2024-09-12 13:45:40 -0300

    endpoint for create user

--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -4,7 +4,7 @@ import "github.com/pcechetto/go-api/internal/entity"
 
 type UserInterface interface {
 	Create(user *entity.User) error
-	Find(email string) (*entity.User, error)
+	FindByEmail(email string) (*entity.User, error)
 }
 
 type ProductInterface interface {
