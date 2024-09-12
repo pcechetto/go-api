548652d44b1fad0ea139f4e1cf24bdb142a06360
pcechetto <pedrocechetto@hotmail.com>
2024-09-12 10:55:32 -0300

    implementando product_db

--- internal/entity/product_test.go
+++ internal/entity/product_test.go
@@ -7,35 +7,35 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	product, err := NewProduct("test", 10)
+	product, err := NewProduct("test", 10.00)
 	assert.Nil(t, err)
 	assert.NotNil(t, product)
 	assert.NotEmpty(t, product.ID)
 	assert.Equal(t, "test", product.Name)
-	assert.Equal(t, 10, product.Price)
+	assert.Equal(t, 10.00, product.Price)
 	assert.NotEmpty(t, product.CreatedAt)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	product, err := NewProduct("", 10)
+	product, err := NewProduct("", 10.00)
 	assert.Equal(t, ErrNameIsRequired, err)
 	assert.Nil(t, product)
 }
 
 func TestProductWhenPriceIsRequired(t *testing.T) {
-	product, err := NewProduct("test", 0)
+	product, err := NewProduct("test", 0.0)
 	assert.Equal(t, ErrPriceIsRequired, err)
 	assert.Nil(t, product)
 }
 
 func TestProductWhenPriceMustBeGreaterThanZero(t *testing.T) {
-	product, err := NewProduct("test", -10)
+	product, err := NewProduct("test", -10.00)
 	assert.Equal(t, ErrPriceMustBeGreaterThanZero, err)
 	assert.Nil(t, product)
 }
 
 func TestProductValidate(t *testing.T) {
-	product, err := NewProduct("test", 10)
+	product, err := NewProduct("test", 10.00)
 	assert.Nil(t, err)
 	assert.NotNil(t, product)
 	assert.Nil(t, product.Validate())
--- internal/infra/database/interfaces.go
+++ internal/infra/database/interfaces.go
@@ -5,4 +5,12 @@ import "github.com/pcechetto/go-api/internal/entity"
 type UserInterface interface {
 	Create(user *entity.User) error
 	Find(email string) (*entity.User, error)
-} 
+}
+
+type ProductInterface interface {
+	Create(product *entity.Product) error
+	FindByID(id string) (*entity.Product, error)
+	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	Update(product *entity.Product) error
+	Delete(id string) error
+}
